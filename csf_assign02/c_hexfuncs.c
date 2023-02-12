39dea7f9c439d8b6e827a97b0255cfcf0a8f9910
bella-xia <zxia15@jh.edu>
2023-02-12 09:28:08 -0500

    modified c_hexfuncs.c

--- csf_assign02/c_hexfuncs.c
+++ csf_assign02/c_hexfuncs.c
@@ -1,6 +1,16 @@
 // C implementation of hexdump functions
 
-#include <unistd.h>  // this is the only system header file you may include!
 #include "hexfuncs.h"
+#include <unistd.h> // this is the only system header file you may include!
 
+char hex_to_printable(unsigned char byteval) {
+  // if the passed-in character is within the range of printable character
+  // (between 33 and 126 in ASCII table), then the original character will be
+  // returned. Otherwise '.' will be returned.
+  if (byteval >= 33 || byteval <= 126) {
+    return byteval;
+  } else {
+    return '.';
+  }
+}
 // TODO: add function implementations here
