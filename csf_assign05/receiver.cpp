fd8baf915cb9483538438fa8f325eee4a036ec01
bella-xia <zxia15@jh.edu>
2023-04-12 20:37:28 -0400

    created files

--- csf_assign05/receiver.cpp
+++ csf_assign05/receiver.cpp
@@ -7,63 +7,77 @@
 #include "connection.h"
 #include "client_util.h"
 
-void error (const char *msg) {
-  std::cout << msg << std::endl;
+void error(const char *msg)
+{
+  std::cout << msg;
   exit(1);
 }
-int main(int argc, char **argv) {
-  if (argc != 5) {
+int main(int argc, char **argv)
+{
+  if (argc != 5)
+  {
     std::cerr << "Usage: ./receiver [server_address] [port] [username] [room]\n";
     return 1;
   }
   std::string port = argv[2];
   std::string server_hostname = argv[1];
-  int server_port = std::stoi(argv[2]);
+  // int server_port = std::stoi(argv[2]);
   std::string username = argv[3];
   std::string room_name = argv[4];
   Connection conn;
   int fd = open_clientfd(server_hostname.c_str(), port.c_str());
-  if (fd < 0) {
-    error("Error: could not connect to server.");
+  if (fd < 0)
+  {
+    error("Error: could not connect to server.\n");
   }
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
-  
+
   std::string rlogin_message = "rlogin:" + username + "\n";
-  std::string join_message = "join:" + room_name;
-  rio_writen(fd, rlogin_message.c_str(), sizeof(rlogin_message));
+  std::string join_message = "join:" + room_name + "\n";
+  rio_writen(fd, rlogin_message.c_str(), rlogin_message.length());
   rio_t rio;
   rio_readinitb(&rio, fd);
   char buf[Message::MAX_LEN + 1];
   ssize_t n = rio_readlineb(&rio, buf, Message::MAX_LEN);
   buf[n] = '\0';
-  if (n <= 0) {
-    error("Error: not reived from server.");
-  } else if (n >= 4 && ((std::string) buf).substr(0, 4) == "err:") {
-    error(buf);
+  if (n <= 0)
+  {
+    error("Error: not reived from server.\n");
   }
-  rio_writen(fd, join_message.c_str(), sizeof(join_message));
+  else if (n >= 4 && ((std::string)buf).substr(0, 4) == "err:")
+  {
+    error(((std::string)buf).substr(4).c_str());
+  }
+  rio_writen(fd, join_message.c_str(), join_message.length());
   n = rio_readlineb(&rio, buf, Message::MAX_LEN);
   buf[n] = '\0';
-  if (n <= 0) {
-    error("Error: not reived from server.");
-  } else if (n >= 4 && ((std::string) buf).substr(0, 4) == "err:") {
-    error(buf);
+  if (n <= 0)
+  {
+    error("Error: not received from server.\n");
+  }
+  else if (n >= 4 && ((std::string)buf).substr(0, 4) == "err:")
+  {
+    error(((std::string)buf).substr(4).c_str());
   }
 
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
 
-  while(1) {
+  while (1)
+  {
     n = rio_readlineb(&rio, buf, Message::MAX_LEN);
     buf[n] = '\0';
-    if (n >= 9 && ((std::string) buf).substr(0, 9) == "delivery:") {
+    if (n >= 9 && ((std::string)buf).substr(0, 9) == "delivery:")
+    {
+      std::cout << ((std::string)buf).substr(9 + room_name.length() + 1);
       continue;
     }
-    else {
-      error("Error: fail to receive delivered message.");
+    else if (n >= 4 && ((std::string)buf).substr(0, 4) == "err:")
+    {
+      // std::cout << ((std::string)buf).substr(4).c_str();
+      error(((std::string)buf).substr(4).c_str());
     }
   }
   return 0;
 }
-
--- csf_assign05/sender.cpp
+++ csf_assign05/sender.cpp
@@ -7,12 +7,15 @@
 #include "connection.h"
 #include "client_util.h"
 
-void error (const char *msg) {
-  std::cout << msg << std::endl;
+void error(const char *msg)
+{
+  std::cout << msg;
   exit(1);
 }
-int main(int argc, char **argv) {
-  if (argc != 4) {
+int main(int argc, char **argv)
+{
+  if (argc != 4)
+  {
     std::cerr << "Usage: ./sender [server_address] [port] [username]\n";
     return 1;
   }
@@ -22,62 +25,84 @@ int main(int argc, char **argv) {
   std::string username;
   std::string port = argv[2];
   server_hostname = argv[1];
-  server_port = std::stoi(argv[2]);
+  // server_port = std::stoi(argv[2]);
   username = argv[3];
-  
+
   int fd = open_clientfd(server_hostname.c_str(), port.c_str());
-  if (fd < 0) {
+  if (fd < 0)
+  {
     error("Error: could not connect to server.");
   }
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
-  
+
   std::string rlogin_message = "slogin:" + username + "\n";
   std::string leave_message = "leave:\n";
   std::string quit_message = "quit:\n";
   std::string join_message = "join:";
   std::string sendall_message = "sendall:";
 
-  rio_writen(fd, rlogin_message.c_str(), sizeof(rlogin_message));
+  rio_writen(fd, rlogin_message.c_str(), rlogin_message.length());
   rio_t rio;
 
   rio_readinitb(&rio, fd);
   char buf[Message::MAX_LEN + 1];
   ssize_t n = rio_readlineb(&rio, buf, Message::MAX_LEN);
   buf[n] = '\0';
-  if (n <= 0) {
-    error("Error: not reived from server.");
-  } else if (n >= 4 && ((std::string) buf).substr(0, 4) == "err:") {
-    error(buf);
+  if (n <= 0)
+  {
+    error("Error: not reived from server.\n");
+  }
+  else if (n >= 4 && ((std::string)buf).substr(0, 4) == "err:")
+  {
+    error(((std::string)buf).substr(4).c_str());
   }
   std::string input;
-  while (std::cin >> input) {
-    if (input.length() > 6 && input.substr(0, 6) == "/leave") {
-      rio_writen(fd, leave_message.c_str(), sizeof(leave_message));
-    } else if (input.length() > 5 && input.substr(0, 5) == "/join") {
-      join_message += input.substr(6) + "\n";
-      rio_writen(fd, join_message.c_str(), sizeof(join_message));
-    } else if (input.length() > 5 && input.substr(0, 5) == "/quit") {
-      rio_writen(fd, quit_message.c_str(), sizeof(quit_message));
+  while (std::getline(std::cin, input))
+  {
+    if (input.length() >= 6 && input.substr(0, 6) == "/leave")
+    {
+      rio_writen(fd, leave_message.c_str(), leave_message.length());
+    }
+    else if (input.length() >= 5 && input.substr(0, 5) == "/join")
+    {
+      std::string join_message_complete = join_message + input.substr(6) + "\n";
+      rio_writen(fd, join_message_complete.c_str(), join_message_complete.length());
+    }
+    else if (input.length() >= 5 && input.substr(0, 5) == "/quit")
+    {
+      rio_writen(fd, quit_message.c_str(), quit_message.length());
       n = rio_readlineb(&rio, buf, Message::MAX_LEN);
       buf[n] = '\0';
-      if (n <= 0) {
-        error("Error: not reived from server.");
-      } else if (n >= 4 && ((std::string) buf).substr(0, 4) == "err:") {
-        error(buf);
-      } else if (n >= 3 && ((std::string) buf).substr(0, 3) == "ok:") {
+      if (n <= 0)
+      {
+        error("Error: not reived from server.\n");
+      }
+      else if (n >= 4 && ((std::string)buf).substr(0, 4) == "err:")
+      {
+        // std::cout << ((std::string)buf).substr(4).c_str();
+        error(((std::string)buf).substr(4).c_str());
+      }
+      else if (n >= 3 && ((std::string)buf).substr(0, 3) == "ok:")
+      {
         exit(0);
       }
-    } else {
-      sendall_message += input + "\n";
-      rio_writen(fd, sendall_message.c_str(), sizeof(sendall_message));
     }
-      n = rio_readlineb(&rio, buf, Message::MAX_LEN);
-      buf[n] = '\0';
-      if (n <= 0) {
-        error("Error: not reived from server.");
-      } else if (n >= 4 && ((std::string) buf).substr(0, 4) == "err:") {
-        error(buf);
+    else
+    {
+      std::string sendall_message_complete = sendall_message + input + "\n";
+      rio_writen(fd, sendall_message_complete.c_str(), sendall_message_complete.length());
+    }
+    n = rio_readlineb(&rio, buf, Message::MAX_LEN);
+    buf[n] = '\0';
+    if (n <= 0)
+    {
+      error("Error: not reived from server.\n");
+    }
+    else if (n >= 4 && ((std::string)buf).substr(0, 4) == "err:")
+    {
+      std::cout << ((std::string)buf).substr(4).c_str();
+      // error(((std::string)buf).substr(4).c_str());
     }
   }
 
