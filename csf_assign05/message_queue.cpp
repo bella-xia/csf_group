33725c59b81202266ff03f32d789810fa8707301
bella-xia <zxia15@jh.edu>
2023-04-25 11:03:02 -0400

    modified message_queue

--- csf_assign05/message_queue.cpp
+++ csf_assign05/message_queue.cpp
@@ -4,35 +4,40 @@
 #include "message.h"
 #include <iostream>
 
-MessageQueue::MessageQueue() {
-  m_messages = std::deque<Message*>();
+MessageQueue::MessageQueue()
+{
+  m_messages = std::deque<Message *>();
   pthread_mutex_init(&m_lock, NULL);
   sem_init(&m_avail, 0, 0);
   // TODO: initialize the mutex and the semaphore
 }
 
-MessageQueue::~MessageQueue() {
-  for(std::deque<Message*>::iterator it = m_messages.begin(); it != m_messages.end(); it++) {
-    delete(*it);
+MessageQueue::~MessageQueue()
+{
+  for (std::deque<Message *>::iterator it = m_messages.begin(); it != m_messages.end(); it++)
+  {
+    delete (*it);
   }
   sem_destroy(&m_avail);
   pthread_mutex_destroy(&m_lock);
   // TODO: destroy the mutex and the semaphore
 }
 
-void MessageQueue::enqueue(Message *msg) {
-  sem_post(&m_avail);
+void MessageQueue::enqueue(Message *msg)
+{
   pthread_mutex_lock(&m_lock);
   m_messages.insert(m_messages.begin(), msg);
   pthread_mutex_unlock(&m_lock);
-  sem_wait(&m_avail);
+  // sem_wait(&m_avail);
+  sem_post(&m_avail);
   // TODO: put the specified message on the queue
 
   // be sure to notify any thread waiting for a message to be
   // available by calling sem_post
 }
 
-Message *MessageQueue::dequeue() {
+Message *MessageQueue::dequeue()
+{
   struct timespec ts;
   // get the current time using clock_gettime:
   // we don't check the return value because the only reason
@@ -42,22 +47,25 @@ Message *MessageQueue::dequeue() {
 
   // compute a time one second in the future
   ts.tv_sec += 1;
-  
-  while(sem_timedwait(&m_avail, &ts) == -1) {
+
+  while (sem_timedwait(&m_avail, &ts) == -1)
+  {
     continue;
-    //TODO: erro
+    // TODO: erro
   };
+
   // TODO: call sem_timedwait to wait up to 1 second for a message
   //       to be available, return nullptr if no message is available
-
+  // sem_wait(&m_avail);
   // TODO: remove the next message from the queue, return it
   pthread_mutex_lock(&m_lock);
   Message *msg = nullptr;
-  if(!m_messages.empty()) {
+  if (!m_messages.empty())
+  {
     msg = m_messages.back();
     m_messages.pop_back();
   }
   pthread_mutex_unlock(&m_lock);
-  sem_post(&m_avail);
+  // sem_post(&m_avail);
   return msg;
 }
--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -17,14 +17,16 @@
 // Server implementation data types
 ////////////////////////////////////////////////////////////////////////
 
-struct SharedData{
-  Connection* conn;
-  Server* m_server;
-  SharedData(Connection *connection, Server *ser){
+struct SharedData
+{
+  Connection *conn;
+  Server *m_server;
+  SharedData(Connection *connection, Server *ser)
+  {
     conn = connection;
     m_server = ser;
   }
-}; 
+};
 // TODO: add any additional data types that might be helpful
 //       for implementing the Server member functions
 
@@ -32,15 +34,18 @@ struct SharedData{
 // Client thread functions
 ////////////////////////////////////////////////////////////////////////
 
-namespace {
+namespace
+{
   int chat_with_sender(SharedData *, std::string);
   int chat_with_receiver(SharedData *, User *);
-  void *worker(void *arg) {
-    SharedData* shared_data = (SharedData *) arg;
+  void *worker(void *arg)
+  {
+    SharedData *shared_data = (SharedData *)arg;
     Connection *connection = shared_data->conn;
-    Server *ser = shared_data ->m_server;
-    if (pthread_detach(pthread_self())!=0) {
-      std::cerr<<"Error: fail to detach the pthread."<<std::endl;
+    Server *ser = shared_data->m_server;
+    if (pthread_detach(pthread_self()) != 0)
+    {
+      std::cerr << "Error: fail to detach the pthread." << std::endl;
       pthread_exit(nullptr);
     }
     // TODO: use a static cast to convert arg from a void* to
@@ -49,143 +54,199 @@ namespace {
     Message login_message;
     Message ok_message(TAG_OK, "\n");
     Message room_message;
-    if (!connection->receive(login_message)) {
-      std::cerr<<"Error: fail to receive"<<std::endl;
+    if (!connection->receive(login_message))
+    {
+      std::cerr << "Error: fail to receive" << std::endl;
       pthread_exit(nullptr);
-      //TODO: err
+      // TODO: err
     }
-    if (login_message.tag == TAG_RLOGIN || 
-        login_message.tag == TAG_SLOGIN) {
-      if(!connection->send(ok_message)) {
-        std::cerr<<"Error: fail to send message"<<std::endl;
+    if (login_message.tag == TAG_RLOGIN ||
+        login_message.tag == TAG_SLOGIN)
+    {
+      if (!connection->send(ok_message))
+      {
+        std::cerr << "Error: fail to send message" << std::endl;
         pthread_exit(nullptr);
-        //TODO err
+        // TODO err
       }
-    } else {
-        std::cerr<<"Error: invalid message"<<std::endl;
-        pthread_exit(nullptr);
-      //TODO err
+    }
+    else
+    {
+      std::cerr << "Error: invalid message" << std::endl;
+      pthread_exit(nullptr);
+      // TODO err
     }
 
-    if (login_message.tag == TAG_RLOGIN) {
-      if(!connection->receive(room_message)){
-        std::cerr<<"Error: fail to receive message" <<std::endl;
+    if (login_message.tag == TAG_RLOGIN)
+    {
+      if (!connection->receive(room_message))
+      {
+        std::cerr << "Error: fail to receive message" << std::endl;
         pthread_exit(nullptr);
-        //TODO: err
+        // TODO: err
       }
-      if(room_message.tag == TAG_JOIN) {
-        if(!connection->send(ok_message)){
-          std::cerr<<"Error: fail to send message" <<std::endl;
+      if (room_message.tag == TAG_JOIN)
+      {
+        if (!connection->send(ok_message))
+        {
+          std::cerr << "Error: fail to send message" << std::endl;
           pthread_exit(nullptr);
-          //TODO: err
-        } else {
+          // TODO: err
+        }
+        else
+        {
           std::string room_name = room_message.data;
           User *new_user = new User(login_message.data);
           Room *rm = ser->find_or_create_room(room_name);
           rm->add_member(new_user);
-          if (chat_with_receiver(shared_data, new_user) == 0) {
-            delete(new_user);
+          if (chat_with_receiver(shared_data, new_user) == 0)
+          {
+            delete (new_user);
             return nullptr;
-          } else {
-            delete(new_user);
+          }
+          else
+          {
+            delete (new_user);
             pthread_exit(nullptr);
           }
         }
-        
-      } else {
-        std::cerr<<"Error: invalid message."<<std::endl;
+      }
+      else
+      {
+        std::cerr << "Error: invalid message." << std::endl;
         pthread_exit(nullptr);
-        //TODO: err
+        // TODO: err
       }
 
-    // TODO: read login message (should be tagged either with
-    //       TAG_SLOGIN or TAG_RLOGIN), send response
+      // TODO: read login message (should be tagged either with
+      //       TAG_SLOGIN or TAG_RLOGIN), send response
 
-    // TODO: depending on whether the client logged in as a sender or
-    //       receiver, communicate with the client (implementing
-    //       separate helper functions for each of these possibilities
-    //       is a good idea)
-    return nullptr;
-    } else {
-      if (chat_with_sender(shared_data, login_message.data) == 0) {
+      // TODO: depending on whether the client logged in as a sender or
+      //       receiver, communicate with the client (implementing
+      //       separate helper functions for each of these possibilities
+      //       is a good idea)
+      return nullptr;
+    }
+    else
+    {
+      if (chat_with_sender(shared_data, login_message.data) == 0)
+      {
         return nullptr;
       }
     }
     return nullptr;
   }
-  int chat_with_sender(SharedData *shared_data, std::string user_name) {
+  int chat_with_sender(SharedData *shared_data, std::string user_name)
+  {
     Connection *connection = shared_data->conn;
     Server *ser = shared_data->m_server;
     Message ok_message = Message(TAG_OK, "\n");
     Message err_message = Message(TAG_ERR, "\n");
-    Room* rm = nullptr;
-    while(1) {
+    Room *rm = nullptr;
+    while (1)
+    {
       Message *input = new Message();
       bool err = false;
-      if (!connection->receive(*input)) {
-        std::cerr<<"Error: fail to receive message."<<std::endl;
+      if (!connection->receive(*input))
+      {
+        std::cerr << "Error: fail to receive message." << std::endl;
         pthread_exit(nullptr);
-        //TODO err
+        // TODO err
       }
-      if (input->tag == TAG_JOIN) {
-        if (rm != nullptr) {
+      if (input->data.length() > Message::MAX_LEN)
+      {
+        err = true;
+        err_message.data = "the message length exceeds the maximum limit.\n";
+      }
+      else if (input->data.length() == 0)
+      {
+        err = true;
+        err_message.data = "cannot parse the message.\n";
+      }
+      else if (input->tag == TAG_JOIN)
+      {
+        /*
+        if (rm != nullptr)
+        {
           err = true;
           err_message.data = "already in a room.\n";
-          //TODO err
+          // TODO err
         }
+        */
         std::string room_name = input->data;
         rm = ser->find_or_create_room(room_name);
-      } else if (input->tag == TAG_LEAVE) {
-        if (rm == nullptr) {
+      }
+      else if (input->tag == TAG_LEAVE)
+      {
+        if (rm == nullptr)
+        {
           err = true;
           err_message.data = "not in a room.\n";
-          //TODO err
-        } else {
+          // TODO err
+        }
+        else
+        {
           rm = nullptr;
         }
-        
-      } else if (input->tag == TAG_QUIT) {
+      }
+      else if (input->tag == TAG_QUIT)
+      {
         connection->send(ok_message);
         return 0;
-      } else {
-        if (rm == nullptr) {
+      }
+      else
+      {
+        if (rm == nullptr)
+        {
           err = true;
           err_message.data = "not in a room.\n";
-        } else {
+        }
+        else
+        {
           std::string msg = input->data;
           rm->broadcast_message(user_name, msg);
         }
       }
-      if(err) {
-        if(!connection->send(err_message)){
-          std::cerr<<"Error: fail to send message."<<std::endl;
+      if (err)
+      {
+        if (!connection->send(err_message))
+        {
+          std::cerr << "Error: fail to send message." << std::endl;
           pthread_exit(nullptr);
         }
-      } else {
-        if(!connection-> send(ok_message)){
-          std::cerr<<"Error: fail to send message."<<std::endl;
+      }
+      else
+      {
+        if (!connection->send(ok_message))
+        {
+          std::cerr << "Error: fail to send message." << std::endl;
           pthread_exit(nullptr);
         }
-      } 
-    delete(input);
+      }
+      delete (input);
     }
     return 0;
   }
 
-  int chat_with_receiver(SharedData *shared_data, User *user) {
+  int chat_with_receiver(SharedData *shared_data, User *user)
+  {
     Connection *connection = shared_data->conn;
-    while(1) { 
+    while (1)
+    {
       Message *message = user->mqueue.dequeue();
-      if (message == nullptr) {
+      if (message == nullptr)
+      {
         continue;
       }
-      if(!connection->send(*message)) {
-        std::cerr<<"Error: fail to send message."<<std::endl;
-        delete(message);
-        return -1;
-        //TODO: err
+      if (!connection->send(*message))
+      {
+        std::cerr << "Error: fail to send message." << std::endl;
+        delete (message);
+        pthread_exit(nullptr);
+        // return -1;
+        // TODO: err
       }
-      delete(message);
+      delete (message);
     }
     return 0;
   }
@@ -196,22 +257,26 @@ namespace {
 ////////////////////////////////////////////////////////////////////////
 
 Server::Server(int port)
-  : m_port(port)
-  , m_ssock(-1) {
+    : m_port(port), m_ssock(-1)
+{
   m_rooms = RoomMap();
   pthread_mutex_init(&m_lock, NULL);
 }
 
-Server::~Server() {
+Server::~Server()
+{
   pthread_mutex_destroy(&m_lock);
-  for(std::map<std::string, Room *>::iterator it = m_rooms.begin(); it != m_rooms.end(); it++) {
-    delete(it->second);
+  for (std::map<std::string, Room *>::iterator it = m_rooms.begin(); it != m_rooms.end(); it++)
+  {
+    delete (it->second);
   }
 }
 
-bool Server::listen() {
+bool Server::listen()
+{
   int listenfd = open_listenfd(std::to_string(m_port).c_str());
-  if (listenfd < 0) {
+  if (listenfd < 0)
+  {
     return false;
   }
   m_ssock = listenfd;
@@ -220,23 +285,27 @@ bool Server::listen() {
   //       if successful, false if not
 }
 
-void Server::handle_client_requests() {
-  while (1) {
+void Server::handle_client_requests()
+{
+  while (1)
+  {
     int clientfd = accept(m_ssock, NULL, NULL);
-    if (clientfd < 0) {
-      std::cerr<<"Error: fail to establish connection."<<std::endl;
+    if (clientfd < 0)
+    {
+      std::cerr << "Error: fail to establish connection." << std::endl;
       exit(1);
-      //TODO:err
+      // TODO:err
     }
-    Connection* connection = new Connection(clientfd);
-    SharedData* shared_data = new SharedData(connection, this);
+    Connection *connection = new Connection(clientfd);
+    SharedData *shared_data = new SharedData(connection, this);
     pthread_t thr_id;
-    if(pthread_create(&thr_id, NULL, worker, shared_data) != 0) {
-      std::cerr<<"Error: fail to create a pthread."<<std::endl;
-      delete(connection);
-      delete(shared_data);
+    if (pthread_create(&thr_id, NULL, worker, shared_data) != 0)
+    {
+      std::cerr << "Error: fail to create a pthread." << std::endl;
+      delete (connection);
+      delete (shared_data);
       pthread_exit(nullptr);
-      //TODO:error
+      // TODO:error
     }
   }
 
@@ -244,12 +313,14 @@ void Server::handle_client_requests() {
   //       pthread for each connected client
 }
 
-Room *Server::find_or_create_room(const std::string &room_name) {
+Room *Server::find_or_create_room(const std::string &room_name)
+{
   // TODO: return a pointer to the unique Room object representing
   //       the named chat room, creating a new one if necessary
   auto it = m_rooms.find(room_name);
-  if (it != m_rooms.end()) {
-    return it->second; 
+  if (it != m_rooms.end())
+  {
+    return it->second;
   }
   Room *new_room = new Room(room_name);
 
